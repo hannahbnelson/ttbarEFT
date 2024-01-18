b1210699ce8d7456e6aec8846064d8413b827ab8
hannahbnelson <hnelson2@nd.edu>
2024-01-18 10:13:34 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='ttbarEFT', 
+    version=0.0.0,
+    description = 'Analysis code for ttbar EFT analysis',
+    packages=setuptools.find_packages()
+)
