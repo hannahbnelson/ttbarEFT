ef2960c07669eaa6ea6dc4b63684fa2ac340f1f7
hannahbnelson <hnelson2@nd.edu>
2024-01-19 05:48:23 -0500

    fix setuptools install

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='ttbarEFT', 
-    version=0.0.0,
+    version='0.0.0',
     description = 'Analysis code for ttbar EFT analysis',
     packages=setuptools.find_packages()
 )
