c8100fd552619c573cc1697d7b8bda31074d1279
hannahbnelson <hnelson2@nd.edu>
2024-02-23 09:33:28 -0500

    plotter for 1d quad functions with calculated points, html

--- analysis/plot_1d_quad.py
+++ analysis/plot_1d_quad.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 # import pickle #read pickle file
 import coffea
@@ -8,6 +9,7 @@ import topcoffea.modules.eft_helper as efth
 import matplotlib.pyplot as plt #plot histograms
 from matplotlib.backends.backend_pdf import PdfPages
 import topcoffea.modules.utils as utils
+from topcoffea.scripts.make_html import make_html
 import mplhep as hep
 import numpy as np
 
@@ -103,6 +105,78 @@ def make_1d_quad_plot(files, hist_name, wc_range, wc_name):
     plt.close(fig)
 
 
+def make_1d_quad_plot_with_scatter(files, save_dir, hist_name, wc_range, wc_name, scatter_lst):
+    '''
+    Make 1d quadratic plot of wc value versus total event weight
+    Parameters
+    ----------
+    files : list
+        List of .pkl.gz files to run over
+    hist_name : str
+        Histogram name to use to get event weight values
+    wc_range : list
+        List of wc values to calculate histogram event weight at
+    wc_name : str
+        single wc that will be scanned
+    manual_lst : list 
+        [[x-values],[y-values]]
+    '''
+    
+    plot_vals = {}
+
+    for fname in files: 
+        if fname.endswith('.pkl.gz'):
+            label = fname[:-7]
+        else: 
+            label = fname
+        hist = get_hist(fname, hist_name)
+        weights = calc_weight_array(hist, wc_range, wc_name)
+        plot_vals[label] = weights
+    
+    fig, ax = plt.subplots()
+    for item in plot_vals:
+        ax.plot(plot_vals[item][0], plot_vals[item][1], label = item)
+
+    ax.scatter(scatter_lst[0], scatter_lst[1], label = "Dedicated")
+    
+    ax.legend()
+    # ax.legend(loc=(1.04, 0.5))
+    ax.set_xlabel(wc_name)
+    ax.set_ylabel(r"$\sigma_{NP} /\ \sigma_{SM}$")
+    plt.grid(True)
+    figname = "quad_1d_"+wc_name+".png"
+    fig.savefig(os.path.join(save_dir,figname))
+    print("plot saved to: ", figname)
+    plt.close(fig)
+
+
+def get_points_from_txt(fname):
+    
+    with open(fname, "r") as f: 
+        lines = f.read().splitlines() 
+    f.close() 
+
+    dedicated = {}
+    i = 0
+
+    while i < len(lines):
+        first = lines[i].split("=")
+        second = lines[i+1].split("+-")
+
+        name = first[0]
+        xval = float([first[1]][0])
+        yval = float([second[0]][0])
+
+        if name not in dedicated.keys():
+            dedicated[name] = [[xval], [yval]]
+        else: 
+            dedicated[name][0].append(xval)
+            dedicated[name][1].append(yval)
+        i += 2
+
+    return dedicated
+
+
 # TODO: add a check that all files have the same set of wc names, 
 # otherwise not all of the plots will be able to be made
 
@@ -111,7 +185,8 @@ if __name__ == '__main__':
     parser.add_argument('--files', '-f', action='extend', nargs='+', required = True, help = "Specify a list of pkl.gz to run over.")
     parser.add_argument('--hist-name', default = 'sow', help = 'Which histogram to use')
     parser.add_argument('--wc-range', default = 1.0, type = float, help = 'Range for wc calculated. Plot created for [-num, num).')
-    parser.add_argument('--wc-name', action='extend', nargs='+', default = 'all', help = 'WC names to make plots for')
+    parser.add_argument('--wc-name', action='extend', nargs='+', default = None, help = 'WC names to make plots for')
+    parser.add_argument('--outpath',  default=".", help = "The path the output files should be saved to")
 
     args = parser.parse_args()
 
@@ -120,9 +195,7 @@ if __name__ == '__main__':
     hist_name = args.hist_name
     wc_max = args.wc_range
     wc_name = args.wc_name
-
-    # Create range based on wc_max
-    wc_range = np.arange(-wc_max, wc_max, 0.5)
+    save_dir_path = args.outpath
 
     # Get full list of possible wc names from the first file
     temp_hist = get_hist(files[0], hist_name)
@@ -130,11 +203,35 @@ if __name__ == '__main__':
 
     # Fill wc_name list. The default is to make plots for all wc names in the hist, 
     # but can be overridden with a list from command line input 
-    if wc_name == 'all':
+    if wc_name == None:
         wc_list = temp_wc_lst
     else:
         wc_list = wc_name
 
+    fname = "MgXS.txt"
+    scatter_dict = get_points_from_txt(fname)
+
+    for wc in wc_list:
+        if wc == "ctGRe" or "ctGIm":
+            wc_range = np.arange(-1.0, 1.0, 0.25)
+       
+        else:
+            np.arange(-wc_max, wc_max, 0.5)
+
+        scatter_xvals = scatter_dict[wc][0]
+        scatter_yvals = np.divide(np.array(scatter_dict[wc][1]), 49.41)
+        scatter_lst = [scatter_xvals, scatter_yvals]
+        make_1d_quad_plot_with_scatter(files, save_dir_path, hist_name, wc_range, wc, scatter_lst)
+
+    # Make an index.html file if saving to web area
+    if "www" in save_dir_path:
+        make_html(save_dir_path)
+
     # Loop through all wc in list and make a 1d quadratic plot for each
-    for wc in wc_list: 
-        make_1d_quad_plot(files, hist_name, wc_range, wc)
+    # for wc in wc_list: 
+    #     make_1d_quad_plot(files, hist_name, wc_range, wc)
+
+
+
+
+
